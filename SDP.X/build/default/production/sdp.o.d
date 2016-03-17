825771b6301a063d2d7a6e8806d127151aeda937
Alissa Mcgill <alm161@zips.uakron.edu>
2016-03-17 13:21:55 -0400

    some shit

--- SDP.X/build/default/production/sdp.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/sdp.o:  \
-sdp.c  \
-config.h 
--- SDP.X/sdp.c
+++ SDP.X/sdp.c
@@ -9,7 +9,7 @@
  * Created on November 17, 2015, 6:52 PM
  */
 
-#include "xc.h"
+// #include "xc.h"
 #include "config.h"
 
 /*
@@ -22,25 +22,20 @@
  *  C_PWR_ALL =
  * 
  * Currently Used for Testing :
- *  A_1_TRIG = RD0
- *  A_2_TRIG = RD1
- *  A_1_ECHO = RD4
- *  A_2_ECHO = RD5
+ *  A_TRIG_ALL = RD0
+ *  A_1_ECHO = RE0
+ *  A_2_ECHO = RE1
  */
 void _10us_delay(int);
 void ms_delay(int);
 void Receive_from_Sensor(void);
 
 int i;                      //Loop Counter
-int distance = 0;           //Sensor Distance value (inches))
-int avgdist = 0;            //Running average of valid sensor distance values
-
-int initial = 0;            //Used in setting average distance
 int trigdsensor[2] = {0, 0};        //Variable set to determine what sensor is triggered
 
 int main(void) {
     T3CON = 0x8010;        //Timer 3 initialization (used in Receive function)
-    TRISA = 0x0000;        //LEDs FOR EXPLORER LED TESTING REMOVE LATERZ
+    TRISA = 0x000C;        //LEDs FOR EXPLORER LED TESTING REMOVE LATERZ
     TRISD = 0x0000;        //Configure all PORTD pins to output
     TRISE = 0x0003;        //Configure all PORTE pins to output EXCEPT RE0, RE1
     PORTE = 0x0000;
@@ -53,22 +48,21 @@ int main(void) {
         PORTDbits.RD0 = 1;     //Begin Pulse TO Sensor on RD0 (Array 1)
         _10us_delay(1);        //Not exact, but close enough! (~N+1 uS) WRONG
         PORTDbits.RD0 = 0;     //Stop Pulse TO Sensor on RD0 (Array 1)
-        
-        Receive_from_Sensor();  //Return Distance value (inches)
-
-        // Below code compares with 10 Inches     
-              //FAKEPOTHOLE (Closer to Breakout)
-        if (trigdsensor[0] == 1){
-            PORTAbits.RA1 = 1;
+        Receive_from_Sensor();  
+        // Below code compares between 5 and 10 Inches
+              
+        if ((trigdsensor[0] == 0) && (trigdsensor[1] == 0)){
+            PORTA = 0;  //0
         }
-        else if (trigdsensor[0] == 0){
-            PORTAbits.RA1 = 0;  //NOFAKEPOTHOLE (Closer to breakout)
+        else if ((trigdsensor[0] == 0) && (trigdsensor[1] == 1)){
+            PORTA = 2;  //2
         }
-        if (trigdsensor[1] == 1){
-            PORTAbits.RA2 = 1;
+        else if ((trigdsensor[0] == 1) && (trigdsensor[1] == 0)){
+            PORTA = 1;  //1
         }
-        else if (trigdsensor[1] == 0){
-            PORTAbits.RA2 = 0;
+        // Filtering (Ex. Speed Bump)
+        else if ((trigdsensor[0] == 1) && (trigdsensor[1] == 1)){
+            PORTA = 0;  //0
         }
         while (TMR3 < 60000); //Wait till end of 30mS cycle
         ms_delay(20);       //Delay between pulses to get reading back. SHORTEN!
@@ -96,69 +90,69 @@ void ms_delay(int N) {
 	return;
 }
 
-//int Receive_from_Sensor(void) {
-//    int time = 0;
-//    while (PORTE == 0x0000);
-//        TMR3 = 0;              //set timer 3 to 0
-//    while (TMR3 < 2960) {  //  2960 = ~10 inches
-//        if (PORTEbits.RE0 == 0) {
-//           trigdsensor[0] = 1;
-//        }
-//        if (PORTEbits.RE1 == 0) {
-//            trigdsensor[1] = 1;
-//        }
-//    }
+
 //    time = TMR3 * 8 / 16000000; //Time(sec) = TimerVal * prescaler / Fcyc
 //    distance = (int) (time * 10^6) / 148;  //INCHES = Time(uS) / 148
-//                                         
-//    return distance;
-//}
+
 
 void Receive_from_Sensor(void) {
-    int prev0 = 0;
-    int prev1 = 0;
+    int prev0 = 0; //Previous Sensor 0 Value
+    int prev1 = 0; //Previous Sensor 1 Value
     
-    int tstart0;
-    int tstart1;
+    int tstart0;   //Sensor 0 Start Time
+    int tstart1;   //Sensor 1 Start Time
     
-    int tend0 = 0;
-    int tend1 = 0;
+    int tend0 = 0;  //Sensor 0 End Time
+    int tend1 = 0;  //Sensor 1 End Time
+    
+//    int distance0;
+//    int distance1;
+//    
+//    int time0;
+//    int time1;
     
     TMR3 = 0;
     while (TMR3 < 10000) {  // While Time < 5mS
-        if (PORTEbits.RE0 == 1 && prev0 == 0) {
+        if (PORTAbits.RA2 == 1 && prev0 == 0) {
             prev0 = 1;
             tstart0 = TMR3;
         }
-        if (PORTEbits.RE1 == 1 && prev1 == 0) {
+        if (PORTAbits.RA3 == 1 && prev1 == 0) {
             prev1 = 1;
             tstart1 = TMR3;
         }
-        if (PORTEbits.RE0 == 0 && prev0 == 1) {
+        if (PORTAbits.RA2 == 0 && prev0 == 1) {
             prev0 = 0;
             tend0 = TMR3;
         }
-        if (PORTEbits.RE1 == 0 && prev1 == 1) {
+        if (PORTAbits.RA3 == 0 && prev1 == 1) {
             prev1 = 0;
             tend1 = TMR3;
         }
     }
     if (tend0 == 0) {
-        tend0 = TMR3;
+        tend0 = TMR3;   //If sensor times out, don't want to trigger
     }
     if (tend1 == 0) {
-        tend1 = TMR3;
+        tend1 = TMR3;   //If sensor times out, don't want to trigger
     }
-    if (tend0 - tstart0 < 2961) { //2960 ~= 10 inches
-        trigdsensor[0] = 1;
+    
+//    time0 = (tend0 - tstart0) * 8 / 16000000; //Time(sec) = TimerVal * prescaler / Fcyc
+//    time1 = (tend1 - tstart1) * 8 / 16000000; //Time(sec) = TimerVal * prescaler / Fcyc
+//    distance0 = (int) (time0 * 10^6 / 148);
+//    distance1 = (int) (time1 * 10^6 / 148);
+//    
+//    if ((5 < distance0) && (distance0 < 10)) { //2960 ~= 10 inches, 1480 ~= 5 inches
+//        trigdsensor[0] = 1;
+//    }
+//    if ((5 < distance1) && (distance1 < 10)) { //2960 ~= 10 inches, 1480 ~= 5 inches
+//        trigdsensor[1] = 1;
+//    }
+
+    if ((1480 < (tend0-tstart0)) && ((tend0 - tstart0) < 2960)) { //2960 ~= 10 inches, 1480 ~= 5 inches
+    trigdsensor[0] = 1;
     }
-    else if (tend0 - tstart0 > 2961) {
-        trigdsensor[0] = 0;
-    }
-    if (tend1 - tstart1 < 2961) { //2960 ~= 10 inches
+    if ((1480 < (tend1 - tstart1)) && ((tend1 - tstart1) < 2960)) { //2960 ~= 10 inches, 1480 ~= 5 inches
         trigdsensor[1] = 1;
-    }
-    else if (tend1 - tstart1 > 2961) {
-        trigdsensor[1] = 0;
     }
 }
